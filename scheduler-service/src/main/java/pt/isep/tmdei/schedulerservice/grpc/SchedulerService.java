df1df22e883bcdde5aea9634932eb93b41c762a7
José Rodrigues <67165102+souma458@users.noreply.github.com>
2022-05-15 17:55:57 +0100

    Add proto file and base service class

---
+++ scheduler-service/src/main/java/pt/isep/tmdei/schedulerservice/grpc/SchedulerService.java
@@ -0,0 +1,17 @@
+package pt.isep.tmdei.schedulerservice.grpc;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+import pt.isep.tmdei.schedulerservice.CreateDeliveryRequest;
+import pt.isep.tmdei.schedulerservice.CreateDeliveryResponse;
+import pt.isep.tmdei.schedulerservice.SchedulerServiceGrpc;
+
+@GrpcService
+public class SchedulerService extends SchedulerServiceGrpc.SchedulerServiceImplBase {
+
+    @Override
+    public void scheduleDelivery(CreateDeliveryRequest request,
+            io.grpc.stub.StreamObserver<CreateDeliveryResponse> responseObserver) {
+        // TODO: Implement
+    }
+
+}
